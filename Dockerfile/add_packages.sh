57f5035053a2ca11422d95ea4a6bb891b6ca3c02
root <root@laszis64.lunarlab.edeka.net>
2019-07-04 12:38:52 +0200

    modified Dockerfile

---
+++ Dockerfile/add_packages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+zypper --non-interactive --gpg-auto-import-keys ref
+
+zypper --non-interactive in python python-xml aaa_base aaa_base-extras net-tools timezone vim less sudo tar
+
