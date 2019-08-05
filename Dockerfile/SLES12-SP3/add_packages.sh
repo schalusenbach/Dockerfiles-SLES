1a1b50aa1e6cc99dc790b3e28b2fcb3e7aa690bb
schalusenbach <34235666+schalusenbach@users.noreply.github.com>
2019-08-05 11:08:13 +0200

    Update add_packages.sh

--- Dockerfile/SLES12-SP3/add_packages.sh
+++ Dockerfile/SLES12-SP3/add_packages.sh
@@ -3,5 +3,5 @@ set -e
 
 zypper --non-interactive --gpg-auto-import-keys ref
 
-zypper --non-interactive in python python-xml aaa_base aaa_base-extras net-tools timezone vim less sudo tar wget curl
+zypper --non-interactive in python python-xml aaa_base aaa_base-extras net-tools timezone vim less sudo tar wget curl git
 
